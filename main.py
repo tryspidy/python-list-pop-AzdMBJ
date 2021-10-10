25f71b1dc518a2518336ca547ca505c5ce02fcf5
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-10 14:33:18 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Python pop list
+
+some_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] # normal python list
+print(some_list) # prints [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+some_list.pop() # pop() is used to pop out one index from a list (default index in pop is -1)
+print(some_list) # prints [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+=====================================================
+# Output:
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+[1, 2, 3, 4, 5, 6, 7, 8, 9]
